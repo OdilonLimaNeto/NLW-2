63e211f002f6bfefdba5144c6dae86adb509c295
OdilonLimaNeto <odilongeronimoo@gmail.com>
2020-08-07 21:05:24 -0500

    created migrations

--- server/src/utils/convertHourToMinutes.ts
+++ server/src/utils/convertHourToMinutes.ts
@@ -1,1 +1,6 @@
-export function
+export default function convertHourToMinutes(time: string) {
+  const [hour, minutes] = time.split(':').map(Number)
+  const timeInMinutes = (hour * 60) + minutes;
+
+  return timeInMinutes;
+}
